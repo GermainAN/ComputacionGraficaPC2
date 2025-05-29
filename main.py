ed17880064843a5c1166939c7dd3316eafdc1f65
GermainAN <germainchoquechambi5@gmail.com>
2025-05-28 21:54:26 -0500

    actualizacion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tempfile
 import os
-from flask import Flask, request, redirect, send_file
+from flask import Flask, request, redirect, send_file, render_template
 from skimage import io
 import base64
 import glob
@@ -8,106 +8,10 @@ import numpy as np
 
 app = Flask(__name__)
 
-main_html = """
-<html>
-<head></head>
-<script>
-  var mousePressed = false;
-  var lastX, lastY;
-  var ctx;
-
-   function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-   }
-
-  function InitThis() {
-      ctx = document.getElementById('myCanvas').getContext("2d");
-
-
-      numero = getRndInteger(0, 10);
-      letra = letra = ["あ", "い", "う", "え", "お"];
-      random = Math.floor(Math.random() * letra.length);
-      aleatorio = letra[random];
-
-      document.getElementById('mensaje').innerHTML  = 'Dibujando un ' + aleatorio;
-      document.getElementById('numero').value = aleatorio;
-
-      $('#myCanvas').mousedown(function (e) {
-          mousePressed = true;
-          Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-      });
-
-      $('#myCanvas').mousemove(function (e) {
-          if (mousePressed) {
-              Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-          }
-      });
-
-      $('#myCanvas').mouseup(function (e) {
-          mousePressed = false;
-      });
-  	    $('#myCanvas').mouseleave(function (e) {
-          mousePressed = false;
-      });
-  }
-
-  function Draw(x, y, isDown) {
-      if (isDown) {
-          ctx.beginPath();
-          ctx.strokeStyle = 'black';
-          ctx.lineWidth = 11;
-          ctx.lineJoin = "round";
-          ctx.moveTo(lastX, lastY);
-          ctx.lineTo(x, y);
-          ctx.closePath();
-          ctx.stroke();
-      }
-      lastX = x; lastY = y;
-  }
-
-  function clearArea() {
-      // Use the identity matrix while clearing the canvas
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  }
-
-  //https://www.askingbox.com/tutorial/send-html5-canvas-as-image-to-server
-  function prepareImg() {
-     var canvas = document.getElementById('myCanvas');
-     document.getElementById('myImage').value = canvas.toDataURL();
-  }
-
-
-
-</script>
-<body onload="InitThis();">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" ></script>
-    <div align="left">
-      
-    </div>
-    <div align="center">
-        <h1 id="mensaje">Dibujando...</h1>
-        <canvas id="myCanvas" width="200" height="200" style="border:2px solid black"></canvas>
-        <br/>
-        <br/>
-        <button onclick="javascript:clearArea();return false;">Borrar</button>
-    </div>
-    <div align="center">
-      <form method="post" action="upload" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
-      <input id="numero" name="numero" type="hidden" value="">
-      <input id="myImage" name="myImage" type="hidden" value="">
-      <input id="bt_upload" type="submit" value="Enviar">
-      </form>
-    </div>
-</body>
-</html>
-
-"""
 
 @app.route("/")
 def main():
-    return(main_html)
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -130,7 +34,15 @@ def upload():
 @app.route('/prepare', methods=['GET'])
 def prepare_dataset():
     images = []
-    d = ['あ', 'い', 'う', 'え', 'お']
+    d = [
+  "bestia",
+  "neblina",
+  "roca",
+  "agua",
+  "amor",
+  "solar",
+  "flor"
+]
     digits = []
     for digit in d:
       filelist = glob.glob('{}/*.png'.format(digit))
@@ -153,7 +65,15 @@ def download_y():
     return send_file('./y.npy')
 
 if __name__ == "__main__":
-    letras_cirilicas = ['あ', 'い', 'う', 'え', 'お']
+    letras_cirilicas = [
+  "bestia",
+  "neblina",
+  "roca",
+  "agua",
+  "amor",
+  "solar",
+  "flor"
+]
     for letra in letras_cirilicas:
         if not os.path.exists(letra):
             os.mkdir(letra)
